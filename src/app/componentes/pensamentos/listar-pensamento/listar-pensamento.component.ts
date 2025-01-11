f2776d4d988838b3d663e41fd04391778a09f22c
Lucas Santana <lucasdeoliveirasantana874@gmail.com>
2025-01-10 21:49:02 -0300

    listando com *ngFor

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-listar-pensamento',
   templateUrl: './listar-pensamento.component.html',
-  styleUrls: ['./listar-pensamento.component.css']
+  styleUrls: ['./listar-pensamento.component.css'],
 })
 export class ListarPensamentoComponent implements OnInit {
+  listaPensamentos = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css'],
+})
+export class PensamentoComponent implements OnInit {
+  pensamento = {
+    conteudo: 'i love angular',
+    autoria: 'lucas',
+    modelo: 'modelo3',
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
